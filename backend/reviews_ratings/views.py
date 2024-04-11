64e76b5568317fd8323d1f346094789fb26c0380
Aartikhemani <98212178+Aartikhemani@users.noreply.github.com>
2024-04-11 12:38:40 +0200

    views changes

--- backend/reviews_ratings/views.py
+++ backend/reviews_ratings/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import *
-from serializers import RatingSerializer, ReviewSerializer
+from .serializers import RatingSerializer, ReviewSerializer
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
