663adc4bb665bbcd1306656c06aee176697394c4
Aartikhemani <98212178+Aartikhemani@users.noreply.github.com>
2024-04-11 17:11:55 +0200

    chef profile

--- backend/reviews_ratings/views.py
+++ backend/reviews_ratings/views.py
@@ -1,12 +1,11 @@
 from rest_framework import viewsets
 from .models import *
 # <<<<<<< HEAD
-from .serializers import RatingSerializer, ReviewSerializer
 # =======
 from .serializers import RatingSerializer, ReviewSerializer, CommunityOpinionSerializer
+
+
 # >>>>>>> 31ca11baf66af229b91bf75b6aa6bf35a2ac57ec
-
-
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
